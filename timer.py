d267ef36cfbbad7dde63429be49922c0b07cfb61
Saniya Siddiqui <saniya984siddiqui@gmail.com>
2024-08-15 16:20:47 +0500

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("Time's up!")
+
+countdown_time = int(input("Enter timer: "))
+countdown_timer(countdown_time)
+
